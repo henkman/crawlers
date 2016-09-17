069c2a0c9dc1266853c7fdad863306fbc83c8ac0
henkman <henkman@github.com>
2016-09-17 19:53:13 +0200

    added actual url resolver for imgur

--- imgur/main.go
+++ imgur/main.go
@@ -9,31 +9,25 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 var (
-	_url string
+	_album string
+	_image string
 )
 
 func init() {
-	flag.StringVar(&_url, "u", "", "album url")
+	flag.StringVar(&_album, "a", "", "album url")
+	flag.StringVar(&_image, "i", "", "image url")
 	flag.Parse()
 }
 
 func main() {
-	if _url == "" {
+	if _album == "" && _image == "" {
 		flag.Usage()
 		return
-	}
-	var id string
-	{
-		reAlbum := regexp.MustCompile("imgur.com/a/([0-9a-zA-Z]+)$")
-		m := reAlbum.FindStringSubmatch(_url)
-		if m == nil {
-			fmt.Println("url is not a valid album")
-			return
-		}
-		id = m[1]
 	}
 	var c http.Client
 	{
@@ -52,34 +46,59 @@ func main() {
 			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36")
 		return c.Do(req)
 	}
-	{ // get cookies
-		res, err := request(c, "GET", _url, nil)
+	if _album != "" {
+		var id string
+		{
+			reAlbum := regexp.MustCompile("imgur.com/a/([0-9a-zA-Z]+)$")
+			m := reAlbum.FindStringSubmatch(_album)
+			if m == nil {
+				fmt.Println("url is not a valid album")
+				return
+			}
+			id = m[1]
+		}
+		{ // get cookies
+			res, err := request(c, "GET", _album, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res.Body.Close()
+		}
+		res, err := request(c,
+			"GET",
+			fmt.Sprintf("http://imgur.com/ajaxalbums/getimages/%s/hit.json", id),
+			nil)
 		if err != nil {
 			log.Fatal(err)
 		}
+		var data struct {
+			Data struct {
+				Images []struct {
+					Hash string `json:"hash"`
+					Ext  string `json:"ext"`
+				} `json:"images"`
+			} `json:"data"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+			res.Body.Close()
+			log.Fatal(err)
+		}
 		res.Body.Close()
-	}
-	res, err := request(c,
-		"GET",
-		fmt.Sprintf("http://imgur.com/ajaxalbums/getimages/%s/hit.json", id),
-		nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var data struct {
-		Data struct {
-			Images []struct {
-				Hash string `json:"hash"`
-				Ext  string `json:"ext"`
-			} `json:"images"`
-		} `json:"data"`
-	}
-	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
-		res.Body.Close()
-		log.Fatal(err)
-	}
-	res.Body.Close()
-	for _, img := range data.Data.Images {
-		fmt.Printf("http://i.imgur.com/%s%s\n", img.Hash, img.Ext)
+		for _, img := range data.Data.Images {
+			fmt.Printf("http://i.imgur.com/%s%s\n", img.Hash, img.Ext)
+		}
+	} else {
+		res, err := request(c, "GET", _image, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		doc, err := goquery.NewDocumentFromResponse(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+		a := doc.Find(".post-image a")
+		if url, ok := a.Attr("href"); ok {
+			fmt.Println(url)
+		}
 	}
 }
